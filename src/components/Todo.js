53c0f7c9970cfdc8fa99e7edb7eaac795ebcd7a7
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-01-25 19:34:22 +0530

    edit todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import TodoContext from "../context/TodoContex";
 
 const Todo = ({ name, id }) => {
   const { dispatch } = useContext(TodoContext);
+  const [isEdit, setIsEdit] = useState(false);
 
   const deleteTodo = () => {
     dispatch({
       type: "deleteTodo",
-      id
+      id,
+    });
+  };
+  const handleChange = (e) => {
+    dispatch({
+      type: "editTodo",
+      todo: { id: id, name: e.target.value },
     });
   };
   return (
     <div className="todo-list">
-      <p>{name}</p>
-      <button>Edit</button>
+      {!isEdit ? (
+        <p>{name}</p>
+      ) : (
+        <input type="text" value={name} onChange={handleChange} />
+      )}
+
+      <button onClick={() => setIsEdit(!isEdit)}>
+        {!isEdit ? "edit" : "save"}
+      </button>
       <button onClick={deleteTodo}>delete</button>
     </div>
   );
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -3,5 +3,13 @@ export default function todoReducer(state, action) {
     return [...state, action.todo];
   } else if (action.type === "deleteTodo") {
     return state.filter((item) => item.id !== action.id);
+  } else if (action.type === "editTodo") {
+    return state?.map((item) => {
+      if (item.id === action.todo.id) {
+        return action.todo;
+      } else {
+        return item;
+      }
+    });
   }
 }
